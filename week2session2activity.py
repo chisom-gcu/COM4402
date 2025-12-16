4c806276f9fe215df50aa173930316c65963f21b
chisom-gcu <chisomumejionu@gmail.com>
2025-12-16 14:55:17 +0000

    week2session2activity recent ver.

--- week2session2activity.py
+++ week2session2activity.py
@@ -1,30 +1,46 @@
-# ACTIVITY 1
-#print("What is your name?")
-#name = input()
-#print("Hello", name)
+# name = "Chisom"
+# age = 18
+# print(f"Hello {name}, you are {age} years old")
 
-# ACTIVITY 2
-#name = input("Enter your name: ")
-#age = input("Enter you age: ")
-
-#print("Hello", name + ", you are", age, "years old.")
-
-# ACTIVITY 3
-#a = float(input("Enter first number: "))
-#b = float(input("Enter second number: "))
-
-#sum_value = a + b
-#product = a * b
-
-#print("Sum is", sum_value)
-#print("Product is", product)
-
-# ACTIVITY 4
-product_name = input("Enter product name: ")
+# # Receipt with Formatting 1
+item_name = input("Enter  item name: ")
 price = float(input("Enter price: "))
 quantity = int(input("Enter quantity: "))
 
 total_cost = price * quantity
 
-print("You bought", quantity, "of", product_name)
+print("You bought", quantity, "of", item_name)
 print("Total cost is", total_cost)
+
+
+# Receipt with Formatting 2
+item_name = "Coffee"
+price = 2.5
+quantity = 3
+
+total = price * quantity
+
+print(f"You bought, {quantity}, of, {item_name} at {total} each. Total = {total}")
+
+# Age Description
+age = 18
+print(f"You are {age} years old.")
+
+#Tidy Name & Course
+full_name = "Alice Smith"
+course_name ="COM4402"
+
+print(full_name.strip().title())
+print(course_name.strip().upper())
+print(f"Student: {full_name} {course_name}")
+
+
+
+
+
+
+
+
+
+
+
