50ca4437496ead9479130bb78862c4519050bc9f
chisom-gcu <chisomumejionu@gmail.com>
2025-12-16 11:01:09 +0000

    Week 2 Session 2 Activity

---
+++ week2session2activity.py
@@ -0,0 +1,30 @@
+# ACTIVITY 1
+#print("What is your name?")
+#name = input()
+#print("Hello", name)
+
+# ACTIVITY 2
+#name = input("Enter your name: ")
+#age = input("Enter you age: ")
+
+#print("Hello", name + ", you are", age, "years old.")
+
+# ACTIVITY 3
+#a = float(input("Enter first number: "))
+#b = float(input("Enter second number: "))
+
+#sum_value = a + b
+#product = a * b
+
+#print("Sum is", sum_value)
+#print("Product is", product)
+
+# ACTIVITY 4
+product_name = input("Enter product name: ")
+price = float(input("Enter price: "))
+quantity = int(input("Enter quantity: "))
+
+total_cost = price * quantity
+
+print("You bought", quantity, "of", product_name)
+print("Total cost is", total_cost)
